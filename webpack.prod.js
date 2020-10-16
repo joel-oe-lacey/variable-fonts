8a08e0ef59625b78a72edf4a98efec45175c62cc
Joel Lacey <joellaceybusiness@gmail.com>
2020-10-15 21:37:56 -0400

    Add font-loader

--- webpack.prod.js
+++ webpack.prod.js
@@ -21,6 +21,19 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.woff$/,
+                loader: 'url-loader',
+                options: {
+                    // Limit at 50k. Above that it emits separate files
+                    limit: 50000,
+                    // url-loader sets mimetype if it's passed.
+                    // Without this it derives it from the file extension
+                    mimetype: 'application/font-woff',
+                    // Output below fonts directory
+                    name: './fonts/[name].[ext]',
+                },
+            },
+            {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
